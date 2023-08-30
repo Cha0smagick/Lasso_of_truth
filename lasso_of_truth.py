da76d183d225f072da6d2699faaae4cd8ba76ac8
Cha0smagick <alekos20@gmail.com>
2023-08-30 18:07:02 -0400

    Update lasso_of_truth.py
    - Añadidos 4 analisis mas.

--- lasso_of_truth.py
+++ lasso_of_truth.py
@@ -7,6 +7,11 @@ from nltk.probability import FreqDist
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import seaborn as sns
+import numpy as np
+
+# Crear la carpeta "resultados" si no existe
+if not os.path.exists("resultados"):
+    os.mkdir("resultados")
 
 # Descargar recursos de NLTK para el idioma español
 nltk.download('punkt')
@@ -46,91 +51,138 @@ num_frases = len(frases)
 
 # Calcular la polaridad promedio de las oraciones
 polaridades = [sent.sentiment.polarity for sent in analisis.sentences]
-polaridad_promedio = sum(polaridades) / len(polaridades)
+polaridad_promedio = round(sum(polaridades) / len(polaridades), 5)  # Redondear a 5 decimales
 
 # Calcular la subjetividad promedio de las oraciones
 subjetividades = [sent.sentiment.subjectivity for sent in analisis.sentences]
-subjetividad_promedio = sum(subjetividades) / len(subjetividades)
+subjetividad_promedio = round(sum(subjetividades) / len(subjetividades), 5)  # Redondear a 5 decimales
 
 # Calcular la longitud promedio de los párrafos
 longitudes_parrafos = [len(frase.split()) for frase in frases]
-longitud_promedio = sum(longitudes_parrafos) / len(longitudes_parrafos)
+longitud_promedio = round(sum(longitudes_parrafos) / len(longitudes_parrafos), 5)  # Redondear a 5 decimales
 
 # Calcular un score de tono basado en las métricas
-score_tono = (polaridad_promedio + 1) * (1 - subjetividad_promedio) * (1 / (longitud_promedio + 1))
+score_tono = round((polaridad_promedio + 1) * (1 - subjetividad_promedio) * (1 / (longitud_promedio + 1)), 5)  # Redondear a 5 decimales
 
-# Visualización de resultados
-plt.figure(figsize=(12, 12))
+# Carpeta para los resultados
+if not os.path.exists("resultados/imagenes"):
+    os.mkdir("resultados/imagenes")
 
-# Gráfico de barras para palabras clave
-plt.subplot(2, 2, 1)
+# Gráfico de barras para palabras clave y guardar como imagen independiente
+plt.figure(figsize=(6, 6))
 plt.bar(*zip(*palabras_clave))
 plt.title('Palabras Clave')
 plt.xticks(rotation=45)
 plt.xlabel('Palabra')
 plt.ylabel('Frecuencia')
+plt.savefig('resultados/imagenes/palabras_clave.png', bbox_inches='tight')  # Guardar la imagen
 
-# Nube de palabras
+# Nube de palabras y guardar como imagen independiente
 wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(frecuencia)
-plt.subplot(2, 2, 2)
+plt.figure(figsize=(8, 4))
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.title('Nube de Palabras')
 plt.axis('off')
+plt.savefig('resultados/imagenes/nube_palabras.png', bbox_inches='tight')  # Guardar la imagen
 
-# Gráfico de barras para longitud de párrafos
-plt.subplot(2, 2, 3)
+# Gráfico de barras para longitud de párrafos y guardar como imagen independiente
+plt.figure(figsize=(8, 6))
 plt.axhline(y=longitud_promedio, color='red', linestyle='--', label='Longitud Promedio')
 sns.barplot(x=list(range(1, num_frases+1)), y=longitudes_parrafos, palette="viridis")
 plt.title('Longitud de Párrafos')
 plt.xlabel('Párrafo')
 plt.ylabel('Longitud de Palabras')
 plt.legend()
+plt.savefig('resultados/imagenes/longitud_parrafos.png', bbox_inches='tight')  # Guardar la imagen
 
-# Histograma de polaridad de oraciones
-plt.subplot(2, 2, 4)
+# Histograma de polaridad de oraciones y guardar como imagen independiente
+plt.figure(figsize=(8, 6))
 sns.histplot(polaridades, kde=True, color="skyblue")
 plt.title('Distribución de Polaridad de Oraciones')
 plt.xlabel('Polaridad')
 plt.ylabel('Frecuencia')
+plt.savefig('resultados/imagenes/histograma_polaridad.png', bbox_inches='tight')  # Guardar la imagen
+
+# Diagrama de dispersión de palabras clave
+# Mostrar la ubicación de las palabras clave en el texto
+plt.figure(figsize=(10, 6))
+for palabra, _ in palabras_clave:
+    posiciones = [i for i, x in enumerate(palabras) if x == palabra]
+    plt.scatter(posiciones, [frecuencia[palabra]] * len(posiciones), label=palabra, alpha=0.5, marker='o')
+plt.title('Diagrama de Dispersión de Palabras Clave')
+plt.xlabel('Posición en el Texto')
+plt.ylabel('Frecuencia')
+plt.legend()
+plt.savefig('resultados/imagenes/diagrama_dispersión_palabras_clave.png', bbox_inches='tight')
+
+# Gráfico de líneas de frecuencia de palabras a lo largo del texto
+frecuencia_palabras = [frecuencia[palabra] for palabra, _ in palabras_clave]
+posiciones_palabras = [pos[0] for pos in palabras_clave]
+plt.figure(figsize=(10, 6))
+plt.plot(posiciones_palabras, frecuencia_palabras, marker='o')
+plt.title('Gráfico de Frecuencia de Palabras Clave a lo Largo del Texto')
+plt.xlabel('Posición en el Texto')
+plt.ylabel('Frecuencia')
+plt.xticks(posiciones_palabras, [palabra for palabra, _ in palabras_clave], rotation=45)
+plt.tight_layout()
+plt.savefig('resultados/imagenes/grafico_frecuencia_palabras.png', bbox_inches='tight')
+
+# Mapa de calor de la matriz de co-ocurrencia
+# Crear una matriz de co-ocurrencia
+co_ocurrencia_matrix = np.zeros((len(palabras_clave), len(palabras_clave)))
+for i, (palabra1, _) in enumerate(palabras_clave):
+    for j, (palabra2, _) in enumerate(palabras_clave):
+        co_ocurrencia = sum(1 for k in range(len(palabras) - 1) if palabras[k] == palabra1 and palabras[k + 1] == palabra2)
+        co_ocurrencia_matrix[i][j] = co_ocurrencia
+
+# Crear el mapa de calor
+plt.figure(figsize=(8, 8))
+sns.heatmap(co_ocurrencia_matrix, annot=True, fmt='.0f', xticklabels=[palabra for palabra, _ in palabras_clave],
+            yticklabels=[palabra for palabra, _ in palabras_clave])
+plt.title('Mapa de Calor de Co-ocurrencia de Palabras Clave')
+plt.savefig('resultados/imagenes/mapa_calor_coocurrencia.png', bbox_inches='tight')
+
+# Gráfico de densidad de polaridad
+plt.figure(figsize=(8, 6))
+sns.kdeplot(polaridades, shade=True, color="skyblue")
+plt.title('Gráfico de Densidad de Polaridad')
+plt.xlabel('Polaridad')
+plt.ylabel('Densidad')
+plt.savefig('resultados/imagenes/grafico_densidad_polaridad.png', bbox_inches='tight')
 
 # Ajustar el espaciado entre subplots
 plt.tight_layout()
 
-# Mostrar análisis de sentimiento
-descripcion_polaridad = "La polaridad mide el tono del texto, donde -1 es muy negativo, 0 es neutro y 1 es muy positivo."
-descripcion_subjetividad = "La subjetividad mide la objetividad del texto, donde 0 es muy objetivo y 1 es muy subjetivo."
-descripcion_longitud = "La longitud promedio de los párrafos se compara con el tamaño promedio en un texto típico."
-
-print("\nAnálisis de Sentimiento:")
-print("Polaridad Promedio de Oraciones:", polaridad_promedio)
-print("Subjetividad Promedio de Oraciones:", subjetividad_promedio)
-print("Longitud Promedio de Párrafos:", longitud_promedio)
-print("\nDescripciones:")
-print(descripcion_polaridad)
-print(descripcion_subjetividad)
-print(descripcion_longitud)
-
-# Definir el tono basado en el score
+# Calcular la conclusión
 if score_tono > 0.7:
     tono = "Muy Positivo"
 elif score_tono > 0.4:
     tono = "Positivo"
 elif score_tono > 0.2:
-    tono = "Neutro Positivo"
+    tono = "Levemente Positivo"
 elif score_tono > -0.2:
     tono = "Neutro"
 elif score_tono > -0.4:
-    tono = "Neutro Negativo"
+    tono = "Levemente Negativo"
 elif score_tono > -0.7:
     tono = "Negativo"
 else:
     tono = "Muy Negativo"
 
-# Conclusión con precisión de 5 dígitos en el score
-conclusion = f"El texto tiene un tono {tono} con un score de tono de {score_tono:.5f}."
+conclusion = f"El tono del texto es {tono} con un score de tono de {score_tono}."
 
-print("\nConclusión:")
-print(conclusion)
+# Guardar análisis de sentimiento en un archivo .txt
+with open('resultados/analisis_sentimiento.txt', 'w', encoding='utf-8') as archivo_txt:
+    archivo_txt.write("Análisis de Sentimiento:\n")
+    archivo_txt.write(f"Polaridad Promedio de Oraciones: {polaridad_promedio}\n")
+    archivo_txt.write(f"Subjetividad Promedio de Oraciones: {subjetividad_promedio}\n")
+    archivo_txt.write(f"Longitud Promedio de Párrafos: {longitud_promedio}\n\n")
+    archivo_txt.write("Descripciones:\n")
+    archivo_txt.write("La polaridad mide el tono del texto, donde -1 es muy negativo, 0 es neutro y 1 es muy positivo.\n")
+    archivo_txt.write("La subjetividad mide la objetividad del texto, donde 0 es muy objetivo y 1 es muy subjetivo.\n")
+    archivo_txt.write("La longitud promedio de los párrafos se compara con el tamaño promedio en un texto típico.\n\n")
+    archivo_txt.write("Conclusión:\n")
+    archivo_txt.write(conclusion)
 
 # Mostrar la figura
 plt.show()
